9ae3a2f6ddddd1055f6ce3de3246d69ca33e05ea
Charles Brown <carbonrobot@gmail.com>
2014-04-28 20:53:52 -0500

    Update paths

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var express = require('express'),
 
 var server = 'mongodb://127.0.0.1:27017/ronindb';
 
-var getProfile = function (id, callback) {
+var getProfile = function (key, callback) {
     mongo.connect(server, function (err, db) {
         var collection = db.collection('profiles');
-        collection.findOne({ id: id }, function (err, doc) {
+        collection.findOne({ key: key }, function (err, doc) {
             callback(doc);
             db.close();
         });
@@ -21,22 +21,34 @@ router.get('/', function (req, res) {
 });
 
 // view profile
-router.get('/:id/view', function (req, res) {
+router.get('/:key/view', function (req, res) {
     getProfile(req.params.id, function (result) {
-        res.render('view', { id: req.params.id, value: JSON.stringify(result, null, '\t') });
+        res.render('view', { key: req.params.key, value: JSON.stringify(result, null, '\t') });
     });
 });
 
-// get profile
-router.get('/:id', function (req, res) {
-    getProfile(req.params.id, function (result) {
+// get complete profile
+router.get('/:key', function (req, res) {
+    getProfile(req.params.key, function (result) {
+        res.send(result);
+    });
+});
+
+// get partial profile
+router.get('/:key/app/:appid', function (req, res) {
+    getProfile(req.params.key, function (result) {
+        for (var i = 0; i < result.apps.length; i++) {
+            if (result.apps[i].key != req.params.appid) {
+                results.apps.splice(i, 1);
+            }
+        }
         res.send(result);
     });
 });
 
 // update profile
-router.post('/:id', function (req, res) {
-    res.send({ "v": "post", "id": req.params.id });
+router.post('/:key/:setting', function (req, res) {
+    res.send({ "setting": req.params.setting, "req": req });
 });
 
 module.exports = router;
